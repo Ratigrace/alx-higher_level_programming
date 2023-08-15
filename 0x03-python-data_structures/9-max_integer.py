2b284d5c27b97ed6e22884ba927dc5f2376a283f
Ratigrace <chijongwegrace@gmail.com>
2023-08-15 11:49:05 -0700

    Max int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-
-    if len(my_list) == 0:
+    if not my_list:
         return None
-
-    max_int = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > max_int:
-            max_int = my_list[i]
-
-            return max_int
+    
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    
+    return max_value
