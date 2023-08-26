3ecf852b5820d82ff086b32ed8a9577e3a7ed245
Ratigrace <chijongwegrace@gmail.com>
2023-08-26 13:31:00 -0700

    name main

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-total = 0
-
-for arg in sys.argv[1:]:
-    total += int(arg)
-
-print(total)
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
