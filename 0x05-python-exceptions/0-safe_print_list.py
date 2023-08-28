782df498cfa858610cfdcfb0f52ec435066fc3b2
Ratigrace <chijongwegrace@gmail.com>
2023-08-28 05:17:33 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    elements_printed = 0
+
+    try:
+        while elements_printed < x:
+            print(my_list[elements_printed], end ="")
+            elements_printed += 1
+    except IndexError:
+            pass
+
+    print()
+    return elements_printed
