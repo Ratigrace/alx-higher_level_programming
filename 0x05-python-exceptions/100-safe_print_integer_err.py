3362db832fa3ef9f676cbc02b461b3cc412b674e
Ratigrace <chijongwegrace@gmail.com>
2023-08-28 14:27:48 -0700

    Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        print()  # Print a new line
-        return True
-    except:
-        error_message = "Exception: Unable to print value as an integer"
-        print(error_message, file=sys.stderr)
-        return False
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
