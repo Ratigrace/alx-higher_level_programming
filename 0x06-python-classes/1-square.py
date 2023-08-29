ba8dd66f17847e88bcb79cba001732213c8cfe1d
Ratigrace <chijongwegrace@gmail.com>
2023-08-29 13:57:24 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''A script that executes python'''
+
+
+class Square:
+    '''defining square'''
+
+    def __init__(self, size):
+        '''Initializing a new Sqaure
+
+        Args:
+        size (int): The legth of each side the new square
+        '''
+
+        self.__size = size
