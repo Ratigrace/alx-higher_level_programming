8947ef00d8590817cd9d1e4b9a5893d6a48c0123
Ratigrace <chijongwegrace@gmail.com>
2023-10-19 13:08:10 -0700

    Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- script that lists the number of records with the same score in the table
+-- The result should display score and number of records with label number
+-- The list should be sorted by the number of records (descending)
+SELECT score , COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
