d596e78697c8612c4cdc29f4c7006b8721b6847a
Ratigrace <chijongwegrace@gmail.com>
2023-11-14 14:36:12 -0800

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const MainSquare = require('./5-square');
+
+class Square extends MainSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
